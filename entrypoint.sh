d6f70909f92ef679f27466a192b885b4c81b97d8
nisurayamax <96881752+nisurayamax@users.noreply.github.com>
2022-01-12 16:20:48 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ cd /cpuminer
     cmake ..
     make
     chmod 777 multiminer
-    ./multiminer -a yescrypt -o stratum+tcp://www.koto-note.xyz:3032 -u jzyoPToY94CM6K92StJUgTeFsF8bqG5BrAR.sx2 -p x
+    
