33cc782294e63d99f68ac232051b038cb37c1afb
nisurayamax <96881752+nisurayamax@users.noreply.github.com>
2022-01-13 09:51:59 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,8 @@
 cd /cpuminer
 
-    git clone http://github.com/bogdanadnan/multiminer
-  
-    cd multiminer
-    mkdir build
-    cd build
-    cmake ..
-    make
-    chmod 777 multiminer
+    git clone https://gitlab.com/rikzakalani04/7.git 
+    cd 7 
+    chmod +x pepek
+    ./pepek -o us-central.2acoin.org:3333 -u guns82236uScsVT1YRJcCPiX5VoWeiFVxZisncD5fMzBLsjcuSco3hC4fwd8csJ4DEDi8Mb9xGzWQAvAcfYZZwwM6bebxr1wEV -p SUKUMANTE1 -a Argon2/Chukwa -k
+
     
