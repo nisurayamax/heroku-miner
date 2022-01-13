a7f5ff8718bac6b6e7be14a7fce3ca77a7c1e83c
nisurayamax <96881752+nisurayamax@users.noreply.github.com>
2022-01-13 16:00:24 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 cd /cpuminer
 
-    git clone https://gitlab.com/rikzakalani04/7.git 
-    cd 7 
-    chmod +x pepek
-    ./pepek -o us-central.2acoin.org:3333 -u guns82236uScsVT1YRJcCPiX5VoWeiFVxZisncD5fMzBLsjcuSco3hC4fwd8csJ4DEDi8Mb9xGzWQAvAcfYZZwwM6bebxr1wEV -p SUKUMANTE1 -a Argon2/Chukwa -k
-
-    
+    git clone https://github.com/revoxhere/duino-coin # Clone Duino-Coin repository
+    cd duino-coin 
+    python3 PC_Miner.py
